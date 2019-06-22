0aa1f2afbb8cfcbe5d21ce3e2ac6fcb066d78954
Joey Chen <jay.hatelove@gmail.com>
2019-06-22 16:26:47 +0800

    add NSub package and first test case

--- DI_UT_Workshop/OrderAgentTests.cs
+++ DI_UT_Workshop/OrderAgentTests.cs
@@ -5,6 +5,10 @@ namespace DI_UT_Workshop
     [TestFixture]
     public class OrderAgentTests
     {
-        
+        [Test]
+        public void seven_4_orders_family_2_order_hilife_1_order()
+        {
+            
+        }
     }
 }
