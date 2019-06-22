51fc4f35e602d7459f6b688244259590deb28621
Joey Chen <jay.hatelove@gmail.com>
2019-06-22 15:12:13 +0800

    Add project files.

---
+++ DI_UT_Workshop/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_UT_Workshop
+{
+    public class Class1
+    {
+    }
+}
